d271e91d44bf76e95a5c87e536d70c9a85de437c
James Knight <knight.of.design@gmail.com>
2025-01-13 20:52:55 -0500

    Incoporate react-chart-js-2

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,57 @@
+'use client'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Energy Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Energy Used (kwh)',
+      data: labels.map(() => 1000),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ]
+}
 export default function Home() {
   return (
     
       <main >
-        <h1>Welcome to Energize</h1>
+        <Line options={options} data={data} />;
       </main>
     
   );
